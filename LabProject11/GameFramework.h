ce1cbd07d1780612916e5e374603929b36f75e0a
joon-so(desktop) <joonso16@gmail.com>
2020-06-29 23:57:32 +0900

    따라하기 12 LabProject11 변경X
    시점변경안됨

---
+++ LabProject11/GameFramework.h
@@ -0,0 +1,90 @@
+#include "Timer.h"
+#include "Scene.h"
+#include "Camera.h"
+#include "Player.h"
+
+class CGameFramework
+{
+private:
+	//다음은 게임 프레임워크에서 사용할 타이머이다.
+	CGameTimer m_GameTimer;
+
+	//다음은 프레임 레이트를 주 윈도우의 캡션에 출력하기 위한 문자열이다. 
+	_TCHAR m_pszFrameRate[50];
+
+	HINSTANCE m_hInstance;
+	HWND m_hWnd;
+
+	int m_nWndClientWidth;
+	int m_nWndClientHeight;
+
+	IDXGIFactory4* m_pdxgiFactory;
+	IDXGISwapChain3* m_pdxgiSwapChain;
+	ID3D12Device* m_pd3dDevice;
+
+	bool m_bMsaa4xEnable = false;
+
+	UINT m_nMsaa4xQualityLevels = 0;
+
+	static const UINT m_nSwapChainBuffers = 2;
+	UINT m_nSwapChainBufferIndex;
+
+	ID3D12Resource* m_ppd3dRenderTargetBuffers[m_nSwapChainBuffers];
+	ID3D12DescriptorHeap* m_pd3dRtvDescriptorHeap;
+	UINT m_nRtvDescriptorIncrementSize;
+
+	ID3D12Resource* m_pd3dDepthStencilBuffer;
+	ID3D12DescriptorHeap* m_pd3dDsvDescriptorHeap;
+	UINT m_nDsvDescriptorIncrementSize;
+
+	ID3D12CommandQueue* m_pd3dCommandQueue;
+	ID3D12CommandAllocator* m_pd3dCommandAllocator;
+	ID3D12GraphicsCommandList* m_pd3dCommandList;
+
+	ID3D12PipelineState* m_pd3dPipelineState;
+
+	ID3D12Fence* m_pd3dFence;
+	UINT64 m_nFenceValue;
+	HANDLE m_hFenceEvent;
+
+public:
+	CGameFramework();
+	~CGameFramework();
+
+	bool OnCreate(HINSTANCE hInstance, HWND hMainWnd);
+	void OnDestroy();
+
+	void CreateSwapChain();
+	void CreateRtvAndDsvDescriptorHeaps();
+	void CreateDirect3DDevice();
+	void CreateCommandQueueAndList();
+
+	void CreateRenderTargetView();
+	void CreateDepthStencilView();
+	void BuildObjects();
+	void ReleaseObjects();
+
+	void ProcessInput();
+	void AnimateObjects();
+	void FrameAdvance();
+
+	void WaitForGpuComplete();
+
+	void MoveToNextFrame();
+	UINT64 m_nFenceValues[m_nSwapChainBuffers];
+	CScene* m_pScene;
+
+	void OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	void OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	LRESULT CALLBACK OnProcessingWindowMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+
+	CCamera* m_pCamera = NULL;
+
+public:
+	//플레이어 객체에 대한 포인터이다.
+	CPlayer* m_pPlayer = NULL;
+
+	//마지막으로 마우스 버튼을 클릭할 때의 마우스 커서의 위치이다. 
+	POINT m_ptOldCursorPos; 
+};
+
